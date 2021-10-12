dc2b7a21a8ef64796215ca3e531cd65f7c111db6
Gregory <grigory.bychenok@skoltech.ru>
2021-10-12 12:51:54 +0300

    Photo saving
    Добавил сохранение фото + эффект дисторсии

---
+++ PhotoRenderer/Assets/Scripts/PhotoRenderer.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class PhotoRenderer : MonoBehaviour
+{
+
+    public string screenShotName = "Test";
+    public Camera camera;
+    void Start()
+    {
+        StartCoroutine(MakeScreenShot());
+    }
+
+    IEnumerator MakeScreenShot()
+    {
+        yield return new WaitForEndOfFrame();
+        int width = this.camera.pixelWidth;
+        int height = this.camera.pixelHeight;
+        
+        Texture2D texture = new Texture2D(width, height);
+
+
+        Rect rect = new Rect(0,0,width,height);
+        texture.ReadPixels(rect,0,0);
+        texture.Apply();
+
+        byte[] bytes = texture.EncodeToPNG();
+        
+        File.WriteAllBytes(Application.dataPath + "/ScreenShots/" + screenShotName + ".png", bytes);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(MakeScreenShot());
+        }
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+}

23203433f7bfabcd24be55d6e074cc985d916858
Gregory <grigory.bychenok@skoltech.ru>
2021-10-20 18:35:21 +0300

    добавил двойной рендер

--- PhotoRenderer/Assets/Scripts/PhotoRenderer.cs
+++ PhotoRenderer/Assets/Scripts/PhotoRenderer.cs
@@ -13,6 +13,9 @@ public class PhotoRenderer : MonoBehaviour
     private int index = 0;
     private static readonly int CameraDepthTexture = Shader.PropertyToID("_CameraDepthTexture");
 
+    private int width;
+    private int height;
+    
     private void OnEnable()
     {
         camera.depthTextureMode |= DepthTextureMode.Depth;
@@ -20,23 +23,22 @@ public class PhotoRenderer : MonoBehaviour
 
     void Start()
     {
-        //StartCoroutine(MakeScreenShot());
+        width = this.camera.pixelWidth;
+        height = this.camera.pixelHeight;
+        StartCoroutine(MakeScreenShot());
         StartCoroutine(MakeDepthMap());
     }
 
     IEnumerator MakeDepthMap()
     {
         yield return new WaitForEndOfFrame();
-        int width = this.camera.pixelWidth;
-        int height = this.camera.pixelHeight;
 
         //The depth texture is set as a shader global under the variable name '_CameraDepthTexture' - we can just get a reference to that global
         RenderTexture depth = Shader.GetGlobalTexture (CameraDepthTexture) as RenderTexture;
         //The depth is technically stored in the depth buffer which we can't access directly, however if we blit that texture into another texture it will copy the depth into the red colour channel
         RenderTexture tmp = RenderTexture.GetTemporary (width, height, 16, RenderTextureFormat.ARGB32);
         Graphics.Blit (depth, tmp);
-
-        yield return null;
+        
         
         //Store the last active render texture and set our depth copy as active
         RenderTexture lastActive = RenderTexture.active;
@@ -45,12 +47,13 @@ public class PhotoRenderer : MonoBehaviour
         
         Texture2D texture = new Texture2D (width, height, TextureFormat.ARGB32, false);
         texture.ReadPixels (new Rect (0, 0, width, height), 0, 0);
+        //texture.
         texture.Apply ();
         
         byte[] bytes = texture.EncodeToPNG();
 
-        index++;
-        File.WriteAllBytes(Application.dataPath + "/ScreenShots/" + screenShotName + index + ".png", bytes);
+        //index++;
+        File.WriteAllBytes(Application.dataPath + "/OutPut/DepthMaps/" + screenShotName + index + ".png", bytes);
         
         //Restore the active render texture and release our temporary tex
         RenderTexture.active = lastActive;
@@ -60,11 +63,8 @@ public class PhotoRenderer : MonoBehaviour
     IEnumerator MakeScreenShot()
     {
         yield return new WaitForEndOfFrame();
-        int width = this.camera.pixelWidth;
-        int height = this.camera.pixelHeight;
-        
-        Texture2D texture = new Texture2D(width, height);
 
+        Texture2D texture = new Texture2D(width, height);
         Rect rect = new Rect(0,0,width,height);
         
         
@@ -73,15 +73,16 @@ public class PhotoRenderer : MonoBehaviour
 
         byte[] bytes = texture.EncodeToPNG();
 
-        index++;
-        File.WriteAllBytes(Application.dataPath + "/ScreenShots/" + screenShotName + index + ".png", bytes);
+        //index++;
+        File.WriteAllBytes(Application.dataPath + "/OutPut/ScreenShots/" + screenShotName + index + ".png", bytes);
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //StartCoroutine(MakeScreenShot());
+            index++;
+            StartCoroutine(MakeScreenShot());
             StartCoroutine(MakeDepthMap());
         }
     }

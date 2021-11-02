47a261e45b34a628bb77615ca1362a54b40d6f83
Gregory <grigory.bychenok@skoltech.ru>
2021-11-02 19:17:14 +0300

    commit

--- PhotoRenderer/Assets/Scripts/Rend.cs
+++ PhotoRenderer/Assets/Scripts/Rend.cs
@@ -8,19 +8,61 @@ public class Rend : MonoBehaviour {
  
    public Material mat;
    public Shader Shader;
+   
+   public Material mat2;
+   public Shader Shader2;
+
+   public Camera camera;
+   public Camera camera2;
+
+   private RenderTexture main;
 
    private void OnEnable()
    {
        mat = new Material(Shader);
+       mat2 = new Material(Shader2);
+       camera2.cullingMask = 1 << 8;
+       camera.cullingMask = -1;
+       //camera2 = camera;
+       main = RenderTexture.GetTemporary (camera.pixelWidth, camera.pixelHeight, 16, RenderTextureFormat.ARGB32);
    }
 
    void Start()
-    {
-        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+   {
+       camera.depthTextureMode = DepthTextureMode.Depth;
+       camera2.depthTextureMode = DepthTextureMode.Depth;
     }
  
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        Graphics.Blit(source, destination,mat);
+        //main = RenderTexture.active;
+        BackMask(source);
+
+        NewMask(source,destination);
+       
     }
+
+    private void BackMask(RenderTexture source)
+    {
+        RenderTexture Mask = RenderTexture.GetTemporary (camera.pixelWidth, camera.pixelHeight, 16, RenderTextureFormat.ARGB32);
+        Graphics.Blit(source, Mask,mat);
+        mat2.SetTexture("_Tex", Mask);
+    }
+
+    private void NewMask(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, mat2);
+    }
+
+    private void OnPreRender()
+    {
+        
+    }
+
+    private void OnPostRender()
+    {
+       // camera.cullingMask = -1;
+    }
+    
+    
 }
